24a87efcab2038eab2ff52dfcafeb4157a2f14e2
nev2024 <nevbilla@gmail.com>
2025-01-13 22:50:34 +0100

    ES6

--- ES6_basic/4-main.js
+++ ES6_basic/4-main.js
@@ -1,4 +1,4 @@
 import returnHowManyArguments from './4-rest-parameter.js';
 
-console.log(returnHowManyArguments("one")); // 1
-console.log(returnHowManyArguments("one", "two", 3, "4th")); // 4
+console.log(returnHowManyArguments('one')); 
+console.log(returnHowManyArguments('one', 'two', 3, '4th')); 
