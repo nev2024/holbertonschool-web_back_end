9b375a61acf8448b8156c59f317a42817fc5291e
nev2024 <nevbilla@gmail.com>
2025-01-19 22:37:38 +0100

    ES6_promise

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
 /*eslint-disable*/
 export default function uploadPhoto(filename) {
-  return Promise.reject('guillaume.jpg cannot be processed');
+  return Promise.reject('Error: guillaume.jpg cannot be processed');
 }
